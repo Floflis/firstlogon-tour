e3a97d625d51f600f2685cb5a2f9d6b4c352aae3
Daniella Mesquita <mesquitadaniella@proton.me>
2023-09-13 19:14:59 -0300

    complement commit 4261631ccfd9be75c7ef38f07022565b9257e428

--- lib/step_screen.dart
+++ lib/step_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
 
+//import 'step1.dart';
+//import 'step2.dart';
+//import 'step3.dart';
+// import more steps as needed
+
 class StepScreen extends StatefulWidget {
   @override
   _StepScreenState createState() => _StepScreenState();
@@ -38,6 +43,15 @@ class _StepScreenState extends State<StepScreen> {
       // Add more steps here
     ];
   }
+
+//List<Step> _buildSteps(BuildContext context) {
+//  return [
+//    step1(context),
+//    step2(context),
+//    step3(context),
+//    // add more steps here
+//  ];
+//}
 
   @override
   Widget build(BuildContext context) {
