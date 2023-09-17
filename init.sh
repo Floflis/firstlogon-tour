6494b334b1faec6061ca8b5e0f1073055a64cc10
Daniella Mesquita <mesquitadaniella@proton.me>
2023-09-17 03:38:54 -0300

    complement commit c84317cb3f4273aa461a9cec77c1e0a7c665fba4

--- init.sh
+++ init.sh
@@ -3,5 +3,9 @@ flutter create --platforms=linux .
 dart pub global activate flutter_to_debian
 export PATH="$PATH":"$HOME/.pub-cache/bin"
 mkdir include
+
 git clone https://github.com/Floflis/linux-icon-getter.git include/linux-icon-getter
 chmod +x include/linux-icon-getter/linux-icon-getter
+
+git clone https://github.com/Floflis/firstlogon-tour_cli.git include/firstlogon-tour_cli
+chmod +x include/firstlogon-tour_cli/core.sh
